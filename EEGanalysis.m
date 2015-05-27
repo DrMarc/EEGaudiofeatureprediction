9e9dfd494714e7a0668f71ce7e500767de83c39c
DrMarc <marc.schoenwiesner@umontreal.ca>
2015-05-27 12:10:34 +0200

    step 1 merges files now

--- EEGanalysis.m
+++ EEGanalysis.m
@@ -1,11 +1,32 @@
-function EEGanalysis(eegfile,step,badchan)
+function EEGanalysis(eegfile)
+
+% determine step
+step = 1;
+while exist(sprintf('.%s%s_step%i.set',filesep,eegfile(1:end-4),step),'file');
+    step = step + 1;
+end
 
 switch step
     case 1
-        disp('Step 1: Manual pruning of continuous data');
+        disp('Step 1: Merge data sets from same session');
+        [FileName,PathName] = uigetfile('.bdf','Select BDF files to merge:','multiselect','on');
+        nfiles = length(FileName);
+        if ~iscell(FileName); error('No files selected!'); end
         [ALLEEG EEG CURRENTSET ALLCOM] = eeglab;
-        EEG = pop_biosig(eegfile,'channels',[1:3,8:22]);
-        [ALLEEG EEG CURRENTSET] = pop_newset(ALLEEG,EEG,0,'gui','off');
+        for i = 1:nfiles
+            EEG = pop_biosig([PathName FileName{i}],'channels',[1:3,8:22]);
+            [ALLEEG, EEG, CURRENTSET] = eeg_store(ALLEEG,EEG,0);
+        end
+        EEG = pop_mergeset(ALLEEG,[1:nfiles],0);
+        EEG = pop_saveset(EEG,'filename',sprintf('%s_step1.set',[FileName{1}]),'filepath','.');
+        close all;
+        clear all;
+    case 2
+        disp('Step 2: Manual pruning of continuous data');
+        %[ALLEEG EEG CURRENTSET ALLCOM] = eeglab;
+        %EEG = pop_biosig(eegfile,'channels',[1:3,8:22]);
+        EEG = pop_loadset('filename',eegfile);
+        %[ALLEEG EEG CURRENTSET] = pop_newset(ALLEEG,EEG,0,'gui','off');
         labels = {'R5','R3','R1','R2','R4','R6','R8','R7','L7','L5','L4a','L3','L1','L2','L4','L4b','L6','L8'}
         for c=1:length(EEG.chanlocs)
             EEG.chanlocs(c).labels = labels{c}; 
@@ -35,7 +56,7 @@ switch step
         EEG = pop_eegfiltnew(EEG,[],1,8250,true,[],0);
         EEG = pop_eegfiltnew(EEG,[],40,330,0,[],0);
         EEG.setname='Step1result';
-        [ALLEEG EEG CURRENTSET] = pop_newset(ALLEEG,EEG,1,'overwrite','on','gui','off'); 
+        %[ALLEEG EEG CURRENTSET] = pop_newset(ALLEEG,EEG,1,'overwrite','on','gui','off'); 
 
         % now reject by eye
         pop_eegplot(EEG,1,1,1);
@@ -46,12 +67,13 @@ switch step
     case 2
         disp('Step 2: ICA of pruned data');
         % run ICA on pruned data
-        EEG = pop_runica(EEG, 'extended',1,'interupt','on');
+        EEG = pop_runica(EEG,'extended',1,'interupt','off');
         EEG = eeg_checkset(EEG);
         EEG = pop_saveset(EEG,'filename',sprintf('%s_step2.set',eegfile(1:end-4)),'filepath','.');
+        pop_expica(EEG, 'weights', '/Users/marc/Documents/MATLAB/marc_bus1_ICA.txt');
         %EEG = pop_subcomp(EEG,[1 2 3],0);
-
     case 3
+        
 end
 
 % concatenate all recordings, then do ICA on filtered and pruned data
